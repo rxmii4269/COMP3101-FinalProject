44f9a1154236a04478f5dd0eaa15467cc1a97603
Romaine Murray <romaine.murray@mymona.uwi.edu>
2020-11-20 18:18:18 -0500

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+
+app = Flask(__name__)
+
+app.config['DEBUG']=True
+app.config['TEMPLATES_AUTO_RELOAD']=True
+app.config['FLASK_ENV'] = 'development'
+
+
+from app import views
+
+app.run(debug=True)
---
+++ app/views.py
@@ -0,0 +1,24 @@
+from flask import render_template, request, url_for
+
+
+from app import app
+
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also tell the browser not to cache the rendered page. If we wanted
+    to we could change max-age to 600 seconds which would be 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
