bf3653c6a82a3dee46083680c835ee8c0db014cf
Romaine Murray <romaine.murray@mymona.uwi.edu>
2020-11-23 13:59:29 -0500

    added functionality to add and remove rows, added visualize button

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,7 +1,33 @@
-"use strict;"
+"use strict";
 
+let $currTableRow = $("#FCFS #start");
+let curValue = parseInt($currTableRow.text().split("")[1]);
+$(".add-row").click(function () {
+  if ($("#FCFS").is(".active")) {
+    let val = curValue + 1;
+    let row = `<tr> 
+                  <td>P${val}</td>
+                  <td><input class="form-control p-0" type="number" value="${val}"></td>
+                  <td><input class="form-control p-0" type="number"></td> 
+              </tr>`;
+    $("#FCFS-table").append(row);
+    curValue = val;
+  } else if ($("#SJN").is(".active")) {
+    console.log($("#SJN-table"));
+  }
+});
 
+$(".remove-row").click(function () {
+  if ($("#FCFS").is(".active")) {
+    let FCFSTableRows = $("#FCFS-table tr");
 
-function FCFS(processQueue){
-    
-}
+    if (FCFSTableRows.length > 2) {
+      FCFSTableRows.last().remove();
+      
+    }
+  } else if ($("#SJN").is(".active")) {
+    console.log("works");
+  }
+});
+
+function FCFS(readyQueue) {}
